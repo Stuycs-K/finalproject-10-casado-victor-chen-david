f73f645f5c0caeb20a49c3d576e7e93e7f3a57aa
Victor Casado <victor.casado.nyc@gmail.com>
2025-05-18 17:13:19 -0400

    framework

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import math
 import time
+import hmac
+import hashlib
 
-for i in range(3):
+timestep = 30
+codelen = 8
+secret_key = 0xAAAA0000AAAAAAABABB
+
+while(True):
     current_unix_time = time.time()
-    print(math.floor(current_unix_time/30))
+    flooredtime = math.floor(current_unix_time/timestep)
+    #print(flooredtime)
+    #print(secret_key)
+    numbytes = math.floor((secret_key.bit_length() + 7) / 8)
+    
+    hash = hmac.new(secret_key.to_bytes(numbytes, byteorder='big'),
+        flooredtime.to_bytes(8, byteorder='big'), hashlib.sha1).digest()
+    #print(hash)
 
-    secret_key = 0xAAAA0000AAAA
-    print(secret_key)
-    time.sleep(30)
+    truncated = hash[:4]
+    #print(truncated)
+
+    precode = int.from_bytes(truncated, byteorder='big')
+    if(precode < 0):
+        precode = precode * -1
+    #print(precode)
+
+    code = str(precode)
+    if len(code) > codelen:
+        code = code[:codelen]
+
+    while(len(code) < codelen):
+        code = "0" + code
+
+    print(code)
+    time.sleep(timestep)
