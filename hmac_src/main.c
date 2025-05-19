0d329d8ad2d2511dbb36649d12ad158df8543fc4
David Chen <dchen65535@gmail.com>
2025-05-19 15:21:40 -0400

    Implement hex interpretation of key

--- hmac_src/main.c
+++ hmac_src/main.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <openssl/sha.h>
 
 // SHA-1 related byte-count constants, see RFC 2104 for definitions
 #define B 64
 #define L 20
+
+unsigned char hex_to_nibble(unsigned char h) {
+	int b = tolower(h);
+	switch(h) {
+		case '\0':
+			return 0;
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			return (unsigned char)(b - '0');
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+			return (unsigned char)(b - 'a' + 10);
+	}
+	return -1;
+}
 
 int main(int argc, char* argv[]) {
 	if (argc < 3) {
@@ -15,10 +43,28 @@ int main(int argc, char* argv[]) {
 	size_t K_len = (strlen(argv[2]) + 1) / 2;  // 2 hex digits to a byte, rounding up
 	unsigned char* K = malloc(K_len * (sizeof(char)));
 	unsigned char* buf;
+#ifdef DEBUG
 	buf = SHA1(argv[2], strlen(argv[2]), NULL);
 	for (int i = 0; i < 20; i++) {
 		printf("%02x", buf[i]);
 	}
+#endif
+	for (int i = 0; i < strlen(argv[2]); i+=2) {
+		unsigned char buf;
+		for (int j = 0; j < 2; j++) {
+			buf = hex_to_nibble(argv[2][i+j]);
+			if (buf & 11110000) {
+				fprintf(stderr, "Invalid hex input `%c`\n", argv[2][i+j]);
+				return 1;
+			}
+			if (j)
+				K[i / 2] = K[i/2] << 4 | buf;
+			else
+				K[i / 2] = buf;
+			printf("K[%d]: %d %c\n", i/2, K[i/2], K[i/2]);
+		}
+	}
+	printf("'%s'", K);
 	// fopen(argv[1], "rb");
 
 }
