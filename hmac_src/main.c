3139cb070dae6f12b0e3996fe84b45d0c8aad1a2
David Chen <dchen65535@gmail.com>
2025-05-19 23:23:37 -0400

    Implement remaining steps of HMAC definition

--- hmac_src/main.c
+++ hmac_src/main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <openssl/sha.h>
 
-// SHA-1 related byte-count constants, see RFC 2104 for definitions
+#define PANIC() fprintf(stderr, "%s\n", strerror(errno)); return errno;
+
+// see RFC 2104 for definitions
+// SHA-1 related byte-count constants
 #define B 64
 #define L 20
+// RFC 2104 specific constant bytestrings
+#define IPAD 0x36
+#define OPAD 0x5C
+
 
 unsigned char hex_to_nibble(unsigned char h) {
 	int b = tolower(h);
@@ -41,8 +49,8 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "Usage: %s infile hexkey\n", argv[0]);
 		return 1;
 	}
-	size_t K_len = (strlen(argv[2]) + 1) / 2;  // 2 hex digits to a byte, rounding up
-	unsigned char* K = malloc(K_len * (sizeof(char)));
+	size_t len = (strlen(argv[2]) + 1) / 2;  // 2 hex digits to a byte, rounding up
+	unsigned char* K = malloc(len * (sizeof(char)));
 	unsigned char* buf;
 #ifdef DEBUG
 	buf = SHA1(argv[2], strlen(argv[2]), NULL);
@@ -64,24 +72,55 @@ int main(int argc, char* argv[]) {
 				K[i / 2] = buf;
 		}
 	}
-	if (K_len > B) {
+	if (len > B) {
 		buf = K;
-		K = SHA1(K, K_len, malloc(L * sizeof(char)));
-		K_len = L;
+		K = SHA1(K, len, malloc(L * sizeof(char)));
+		len = L;
 		free(buf);
 	}
-	if (K_len < B) {
+	if (len < B) {
 		buf = K;
 		K = calloc(B, sizeof(char));
-		memcpy(K, buf, K_len);
+		memcpy(K, buf, len);
+		free(buf);
 	}
+	// see 2104.2.2-3
+	struct stat stat_buf;
+	if (stat(argv[1], &stat_buf)) {
+		fprintf(stderr, "%s\n", strerror(errno));
+		return errno;
+	};
+
+	len = B + stat_buf.st_size;
+	unsigned char* K_ipad = malloc(sizeof(char) * len);
 	for (int i = 0; i < B; i++)
-		printf("%02x ", K[i]);
+		K_ipad[i] = K[i] ^ IPAD;
 
 	FILE *in;
 	if (!(in = fopen(argv[1], "rb"))) {
-		fprintf(stderr, "%s\n", strerror(errno));
-		return errno;
+		PANIC()
+	}
+	if (fread(K_ipad+B, sizeof(char), stat_buf.st_size, in) != stat_buf.st_size) {
+		PANIC()
 	}
 
+	// see 2104.2.4
+	unsigned char* H_4 = SHA1(K_ipad, len, NULL);
+#ifdef DEBUG
+	for (int i = 0; i < L; i++) {
+		printf("%02x", H_4[i]);
+	}
+#endif
+	len = B + L;
+	unsigned char* K_opad = malloc(len * sizeof(char));
+	for (int i = 0; i < B; i++)
+		K_opad[i] = K[i] ^ OPAD;
+	memcpy(K_opad+B, H_4, L);
+	unsigned char* ret = SHA1(K_opad, len, malloc(L));
+	free(K);
+	free(K_ipad);
+	free(K_opad);
+	for (int i = 0; i < L; i++)
+		printf("%02x", ret[i]);
+	printf("\n");
 }
