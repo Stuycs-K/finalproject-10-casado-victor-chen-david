125bd6c5667b69618b07bbbaaa7d874effe5ecf4
David Chen <dchen65535@gmail.com>
2025-05-22 15:20:35 -0400

    Oh. (Flip Python wrapper arguments)

--- hmac_custom.py
+++ hmac_custom.py
@@ -4,5 +4,5 @@ lib = cdll.LoadLibrary("./hmac_src/hmac_sha.so");
 lib.hmac_sha1.restype = POINTER(c_ubyte)
 
 def hmac_sha1(key: bytes, text: bytes):
-    p = lib.hmac_sha1(text, len(text), key, len(key))
+    p = lib.hmac_sha1(key, len(key), text, len(text))
     return bytearray(p[:20])
