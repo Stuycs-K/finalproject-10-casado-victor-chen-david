0950ce117626b4a0d1e8e9a8df4d06fe01520235
Victor Casado <vcasado50@stuy.edu>
2025-05-23 15:35:26 -0400

    working server

--- server/__init__.py
+++ server/__init__.py
@@ -3,12 +3,20 @@ from main import *
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/verifyCode')
+@app.route('/verifyCode', methods=['POST'])
 def verify():
+    timestep = 30
+    # RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
+    codelen = 6
+    secret_key = 0xAAAA0000AAAAAAABABB
+
+
     # https://github.com/google/google-authenticator/wiki/Key-Uri-Format
     if len(argv) > 1:
         url = urlparse(argv[1])
@@ -31,9 +39,16 @@ def verify():
         if q["digits"]:
             codelen = int(q["digits"][0])
 
-    code = main.generate_code(secret_key, timestep, codelen)
-    print(code)
+    codes = [generate_code(secret_key, timestep, codelen), generate_code(secret_key, timestep, codelen, past=1)]
+    print(codes)
 
+    if request.method == "POST":
+       testcode = request.form.get("code")
+       print(testcode)
+       if codes[0] == testcode or codes[1] == testcode:
+           return ("Code Verified")
+       return ("Bad code")
+    return ("No code inputted")
 
 if __name__ == '__main__':
     app.run(port = 5001)
--- server/main.py
+++ server/main.py
@@ -2,45 +2,49 @@
 import math
 import time
 import hmac
-from hmac_custom import hmac_sha1
+#from hmac_custom import hmac_sha1
 import hashlib
 import subprocess
 from sys import argv
 from urllib.parse import urlparse,parse_qs, unquote
 import base64
 
-timestep = 30
-# RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
-codelen = 6
-secret_key = 0xAAAA0000AAAAAAABABB
-
-def generate_code(secret, timestep, code_len):
+def generate_code(secret, timestep, code_len, past = 0):
     current_unix_time = time.time()
     flooredtime = math.floor(current_unix_time/timestep)
+    flooredtime -= past
 
     if (type(secret) == bytes):
         key = secret
     else:
-        numbytes = math.floor((secret_key.bit_length() + 7) / 8)
+        numbytes = math.floor((secret.bit_length() + 7) / 8)
         key = secret.to_bytes(numbytes, byteorder='big')
     text = flooredtime.to_bytes(8, byteorder='big')
-    hash = hmac_sha1(key, text)
+
+    hash = hmac.new(key,text, hashlib.sha1).digest()
+    #hash = hmac_sha1(key, text)
 
     offset = hash[-1] & 15
     truncated = hash[offset:offset + 4]
 
     precode = int.from_bytes(truncated, byteorder='big') & 2147483647
 
-    code = str(precode % (10 ** codelen))
-    if len(code) > codelen:
-        code = code[:codelen]
+    code = str(precode % (10 ** code_len))
+    if len(code) > code_len:
+        code = code[:code_len]
 
-    while(len(code) < codelen):
+    while(len(code) < code_len):
         code = "0" + code
 
     return code
 
 if __name__ == '__main__':
+
+    timestep = 30
+    # RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
+    codelen = 6
+    secret_key = 0xAAAA0000AAAAAAABABB
+
     # https://github.com/google/google-authenticator/wiki/Key-Uri-Format
     if len(argv) > 1:
         url = urlparse(argv[1])
