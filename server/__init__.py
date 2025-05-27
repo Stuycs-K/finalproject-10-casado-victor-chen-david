7c2be6dc468be970c98b829dfef54d8f4b71d25b
Victor Casado <vcasado50@stuy.edu>
2025-05-27 15:06:24 -0400

    fix2

--- server/__init__.py
+++ server/__init__.py
@@ -15,7 +15,7 @@ def verify():
     # RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
     codelen = 6
     secret_key = 0xAAAA0000AAAAAAABABB
-    codes = [generate_code(secret_key, timestep, codelen), generate_code(secret_key, timestep, codelen, past=1)]
+    codes = [generate_code(secret_key, timestep, codelen), generate_code(secret_key, timestep, codelen, offset=1)]
     print(codes)
 
     if request.method == "POST":
