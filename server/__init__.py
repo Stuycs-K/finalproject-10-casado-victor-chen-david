4daa38cc7c92b01341e5b45edc12c7d7d3d22c67
Victor Casado <vcasado50@stuy.edu>
2025-05-29 15:35:28 -0400

    /.

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from shared.totp import *
+import qrcode
+import os
+import base64
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16).hex()
 
-app.secret_key = 'the random string'
+timestep = 30
+# RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
+codelen = 6
+secret_key = os.urandom(16)
+qrstring = (f'otpauth://totp/Test:Test2?secret={base64.b32encode(secret_key).decode("utf-8")}&issuer=CYBER_HMAC')
+
+img=qrcode.make(qrstring)
+img.save('./server/static/qrcode.png')
 
 @app.route('/')
 def home():
@@ -11,10 +22,7 @@ def home():
 
 @app.route('/verifyCode', methods=['POST'])
 def verify():
-    timestep = 30
-    # RFC 6238 uses 8 in its example implementation, no explicit recommendations; Google defaults to 6 and that appears to be standard everywhere else
-    codelen = 6
-    secret_key = 0xAAAA0000AAAAAAABABB
+
     codes = [generate_code(secret_key, timestep, codelen), generate_code(secret_key, timestep, codelen, offset=1)]
     print(codes)
 
